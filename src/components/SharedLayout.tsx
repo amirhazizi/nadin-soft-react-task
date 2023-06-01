c7ac4d9a7ab4a11605280a75293cb8b15ed604dd
amirhazizi <amirazizihosein@gmail.com>
2023-06-01 23:06:00 +0330

    localStorage added

--- src/components/SharedLayout.tsx
+++ src/components/SharedLayout.tsx
@@ -1,14 +1,37 @@
+import { useEffect } from "react"
+
 import { Outlet, NavLink } from "react-router-dom" // react router
 
-import { useSelector } from "react-redux" // state selector
+import { useSelector, useDispatch } from "react-redux" // state selector
+import { RootState } from "../redux/store" // state type (typescript required)
+import { getFromLocalStorage, initialState } from "../redux/storeSlicer"
 
 import UserModal from "./UserModal" // user modal
 
 import { Container } from "@mui/material" // mui container
-import { RootState } from "../redux/store" // state type (typescript required)
+
+import { useLocalStorage } from "usehooks-ts" // localstorage custom hook
 
 const SharedLayout = () => {
-  const user = useSelector((state: RootState) => state.storeReducer.user) // user state
+  const { user, city, theme, todoList } = useSelector(
+    (state: RootState) => state.storeReducer
+  ) // user state
+  const dispatch = useDispatch() //redux distpatch
+  const [localStorageState, saveLocalStorageState] = useLocalStorage(
+    "nadin-soft-react-test",
+    initialState
+  ) // local Storage
+
+  //update localStorages
+  useEffect(() => {
+    saveLocalStorageState({ user, city, theme, todoList })
+  }, [user, city, theme, todoList, saveLocalStorageState])
+
+  // set localStorage State on redux
+  useEffect(() => {
+    dispatch(getFromLocalStorage(localStorageState))
+  }, [])
+
   return (
     <Container>
       {!user && <UserModal />}
